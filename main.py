dfa3dca8c0fa21f99ce9420833a8938ddd5ca6f3
bruno gomes <bggomes18@gmail.com>
2020-12-07 12:34:16 +0000

    added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((800,800)) 
+
+pygame.display.set_caption("Music Game")
+run = True
+
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            run = False
+    
+    pygame.draw.rect(win, (255, 0, 0), (0,0, 50, 50))
+    pygame.display.update()
+
+pygame.quit()
